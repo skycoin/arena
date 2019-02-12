7ba68ff7d52475e39b9ee9542fdf27b229f9d020
Muhammad Hari Suharto <muharihar@infoterkini.com>
2019-02-12 23:29:53 +0700

    add arena mitchellh/cli and init update go module

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/skycoin/arena
+
+require (
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/mitchellh/cli v1.0.0
+	golang.org/x/sys v0.0.0-20190116161447-11f53e031339 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/mitchellh/cli"
+)
 
 func main() {
-	fmt.Println("Arena")
+	myCli := cli.NewCLI("arena", "1.0.0")
+
+	myCli.Args = os.Args[1:]
+
+	myCli.Commands = map[string]cli.CommandFactory{}
+
+	exitStatus, err := myCli.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	os.Exit(exitStatus)
 }
