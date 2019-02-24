b42e7ea8c46e4d67c30e1b3665b17681c0a178c7
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2019-02-24 13:42:35 +0300

    Add addtwonumbers package, implement AddTwoNumbers function.

---
+++ pkg/addtwonumbers/addtwonumbers.go
@@ -0,0 +1,11 @@
+package addtwonumbers
+
+import (
+	"math/big"
+)
+
+// AddTwoNumbers performs an addition of two numbers, passed as arguments of type `*big.Float` named `n1` and `n2`.
+// It returns a value of type `*big.Float`.
+func AddTwoNumbers(n1, n2 *big.Float) *big.Float {
+	return new(big.Float).Add(n1, n2)
+}
