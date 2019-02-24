ca06b97f8dc10557107769213922e0b08a2af7b2
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2019-02-24 14:19:07 +0300

    Move exit status code literal to const block.

--- cmd/add_two_numbers/add_two_numbers.go
+++ cmd/add_two_numbers/add_two_numbers.go
@@ -10,25 +10,26 @@ import (
 
 const (
 	// Let math/big detect base.
-	base = 0
+	base            = 0
+	exitWithFailure = 1
 )
 
 func main() {
 	if len(os.Args) != 3 {
 		_, _ = fmt.Fprintf(os.Stderr, "Usage: %v <num1> <num2>\n", os.Args[0])
-		os.Exit(1)
+		os.Exit(exitWithFailure)
 	}
 
 	num1, _, err := new(big.Float).Parse(os.Args[1], base)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Could not parse %v: %v\n", os.Args[1], err)
-		os.Exit(1)
+		os.Exit(exitWithFailure)
 	}
 
 	num2, _, err := new(big.Float).Parse(os.Args[2], base)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Could not parse %v: %v\n", os.Args[2], err)
-		os.Exit(1)
+		os.Exit(exitWithFailure)
 	}
 
 	result := service.AddTwoNumbers(num1, num2)
