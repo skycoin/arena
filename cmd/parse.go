4fe11359454faa5ca468113f0305b16a43117779
Avinash U <utekaravinash@gmail.com>
2019-03-06 20:20:45 +0530

    renamed variables

--- cmd/parse.go
+++ cmd/parse.go
@@ -11,13 +11,13 @@ import (
 func Parse() {
 
 	// Accept two values and store them into variables
-	var firstPtr, secondPtr float64
-	flag.Float64Var(&firstPtr, "first", 1.0, "accepts first number")
-	flag.Float64Var(&secondPtr, "second", 1.0, "accepts second number")
+	var first, second float64
+	flag.Float64Var(&first, "first", 1.0, "accepts first number")
+	flag.Float64Var(&second, "second", 1.0, "accepts second number")
 	flag.Parse()
 
 	// Add input values
-	result := math.Add(firstPtr, secondPtr)
+	result := math.Add(first, second)
 
 	// Print result
 	_, err := fmt.Printf("The result is %v\n", result)
