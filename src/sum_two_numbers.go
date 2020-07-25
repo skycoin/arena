2adecf909147bd52e7170a67f890b3524fe670ee
Nair18 <nair.svnit@gmail.com>
2020-07-25 16:36:48 +0530

    code to add two numbers

---
+++ src/sum_two_numbers.go
@@ -0,0 +1,45 @@
+package main
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"errors"
+)
+
+func ErrorFormatter(ErrorMessage string) error {
+	return errors.New(ErrorMessage)
+}
+
+func CheckForInValidInputSize(arr []string) bool {
+	return len(arr) != 2
+}
+
+func addTwoNumbers(NumberArray []string) (float64,error) {
+	var i int
+	var sum float64
+
+	for i = 0; i < len(NumberArray);i++ {
+		var number, err = strconv.ParseFloat(NumberArray[i], 64)
+		if err != nil{
+			return sum,ErrorFormatter("Something went wrong")
+		}
+		sum += number
+	}
+	return sum, nil
+}
+
+func main() {
+	var args = os.Args[1:]
+	if CheckForInValidInputSize(args) {
+		fmt.Println(ErrorFormatter("Invalid input size"))
+		return
+	}
+	var err error
+	var sum float64
+	sum, err = addTwoNumbers(args)
+	if err != nil {
+		fmt.Println(ErrorFormatter("Invalid input"))
+		return
+	}
+	fmt.Println(sum)
+}
