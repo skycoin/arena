18fd746c92f77a323502daa30a9653e467d97ed8
punithsr27 <punithsr27@gmail.com>
2020-07-30 00:16:46 +0530

    updated log msg in test clsaa

--- src/testdata/addition_test.go
+++ src/testdata/addition_test.go
@@ -6,6 +6,6 @@ import "awesomeProject1/src/maths"
 func TestSum(t *testing.T) {
 		actual := maths.Addition{FirstValue: 3, SecondValue: 4}.GetSum()
 		if 7.0000 != actual {
-			t.Errorf("Sum was , got: %f, want: %f", actual, 7.000000)
+			t.Errorf("Mistmatched data: actual: %f, expected: %f", actual, 7.000000)
 		}
 }
