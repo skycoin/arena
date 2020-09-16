2e0bab44de25bca544b3a68e593766b8550ea787
devignesh <vinesh1865@gmail.com>
2020-09-16 18:35:39 +0530

    addition function stuffs

--- golang_task.go
+++ golang_task.go
@@ -5,10 +5,24 @@ import (
 )
 
 func Addition() {
-	fmt.Println("function part")
+	// fmt.Println("function part")
+
+	var First_number, Second_number int
+
+	fmt.Println("Enter your First Number:")
+	fmt.Scanln(&First_number)
+
+	fmt.Println("Enter your Second Number:")
+	fmt.Scanln(&Second_number)
+
+	Result := First_number + Second_number
+
+	fmt.Println("Addition of two numbers:", First_number, "+", Second_number, "=", Result)
+	fmt.Println("Result :", Result)
+
 }
 
 func main() {
-	fmt.Println("main func")
+	// fmt.Println("main func")
 	Addition()
 }
