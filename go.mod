1755c448015c88d7f6521c6914748fe1559cd10e
IvanPav <93catdog@gmai.com>
2020-09-30 22:09:59 +0300

    IvanPavlov_screen-test
    Created program that takes two numbers from the command line, adds them up and prints the result.
    
    Used go modules as mentioned in recommendations.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/catdog93/arena
+
+go 1.15
+
+require github.com/golang/protobuf v1.4.2 // indirect
--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,6 +11,7 @@ import (
 
 const (
 	numberOfInputArguments = 2
+	separator              = " "
 )
 
 func main() {
@@ -18,45 +20,58 @@ func main() {
 	}
 }
 
-// function tries to read arguments from command line, it'll print error if input data format is wrong
-func readArgumentsFromCLI() (numbers []float64, err error) {
-	reader := bufio.NewReader(os.Stdin)
+// function tries to read arguments from command line, it'll return an error if input data format is incorrect
+func readArgumentsFromCLI() (numbers []string, err error) {
 	fmt.Printf("Input format with space is \"number_1 number_1_2\", for example, \"3 -53\"\nEnter %v numbers, please: ", numberOfInputArguments)
+
+	reader := bufio.NewReader(os.Stdin)
 	inputString, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Printf("%v\n\n", err)
 		return nil, err
 	}
 
-	stringArgs := strings.Split(inputString, " ")
-	arguments := make([]float64, len(stringArgs))
+	return strings.Split(inputString, separator), nil
+}
 
-	if len(stringArgs) >= numberOfInputArguments {
+// function tries to sum arguments from command line after successful reading
+func sumVariablesFromCLI() float64 {
+	arguments, err := readArgumentsFromCLI()
+	if err != nil {
+		fmt.Print(err)
+		return 0
+	}
+
+	numbers, err := parseStringToFloat(arguments)
+	if err != nil {
+		fmt.Print(err)
+		return 0
+	}
+
+	sum := sumFloatVariables(numbers)
+	fmt.Printf("Sum of your numbers is: %v\n\n", sum)
+	return sum
+}
+
+// function tries to parse inputStrings to float, it'll return an error if input strings format is incorrect
+func parseStringToFloat(inputStrings []string) (numbers []float64, err error) {
+	arguments := make([]float64, len(inputStrings))
+
+	if len(inputStrings) >= numberOfInputArguments {
 		for i := 0; i < numberOfInputArguments; i++ {
-			stringArg := strings.TrimSpace(stringArgs[i])
+			stringArg := strings.TrimSpace(inputStrings[i])
 			number, err := strconv.ParseFloat(stringArg, 64)
 			if err != nil {
-				fmt.Printf("Input data format is incorrect, please try again\n\n")
+				err := errors.New("Input data format is incorrect, please try again\n\n")
 				return nil, err
 			}
 			arguments = append(arguments, number)
 		}
 	} else {
-		fmt.Printf("Input data format is incorrect, please try again\n\n")
+		err := errors.New("Wrong amount of arguments, please try again\n\n")
 		return nil, err
 	}
 
 	return arguments, nil
-}
-
-// function tries to sum arguments from command line after successful reading
-func sumVariablesFromCLI() {
-	numbers, err := readArgumentsFromCLI()
-	if err != nil {
-		return
-	}
-	sum := sumFloatVariables(numbers)
-	fmt.Printf("Sum of your numbers is: %v\n\n", sum)
 }
 
 // function sums all arguments from input slice
