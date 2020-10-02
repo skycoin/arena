e293ead3a49d341db0291cfc01af89b1e37b6d7b
nvm <nevermn@protonmail.com>
2020-10-02 21:35:59 +0300

    Add and call argument parsing functions

--- cmd/adder/main.go
+++ cmd/adder/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/skycoin/arena/pkg/adder"
+)
 
 func main() {
-	fmt.Println("Adder will be here")
+	a, b, err := parseArguments(os.Args[1:])
+	if err != nil {
+		log.Fatalf("%s\n", err.Error())
+	}
+	adder.AddNumbers(a, b)
+}
+
+var notEnoughArgsErr = errors.New("Not enough arguments")
+
+type parseError struct {
+	value string
+}
+
+func (pe parseError) Error() string {
+	return fmt.Sprintf("Invalid float value: %s", pe.value)
+}
+
+// parseArguments takes first two arguments and tries to parse them
+// into floats
+// return parsed values, or error:
+// notEnoughArgsErr when there are less than two arguments
+// parseFailedErr when one of the two
+func parseArguments(args []string) (float64, float64, error) {
+	var a, b float64
+	if len(args) < 2 {
+		return a, b, notEnoughArgsErr
+	}
+	// use loop and return a slice if the number of arguments to sum becomes higher
+	a, err := parseValue(args[0])
+	if err != nil {
+		return a, b, err
+	}
+	b, err = parseValue(args[1])
+	if err != nil {
+		return a, b, err
+	}
+	return a, b, nil
+}
+
+func parseValue(val string) (float64, error) {
+	panic("Not implemented")
 }
