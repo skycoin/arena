44e054264af50362c70f5f07581c08d00bff2d89
nvm <nevermn@protonmail.com>
2020-10-03 11:17:21 +0300

    Implement parsing

--- cmd/adder/main.go
+++ cmd/adder/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"os"
 
@@ -10,45 +8,12 @@ import (
 )
 
 func main() {
-	a, b, err := parseArguments(os.Args[1:])
+	a, b, err := adder.ParseArguments(os.Args[1:])
 	if err != nil {
-		log.Fatalf("%s\n", err.Error())
+		log.Fatal(err.Error())
+	}
+	if a.IsInf() || b.IsInf() {
+		log.Fatal("Error: infinities aren't allowed")
 	}
 	adder.AddNumbers(a, b)
-}
-
-var notEnoughArgsErr = errors.New("Not enough arguments")
-
-type parseError struct {
-	value string
-}
-
-func (pe parseError) Error() string {
-	return fmt.Sprintf("Invalid float value: %s", pe.value)
-}
-
-// parseArguments takes first two arguments and tries to parse them
-// into floats
-// return parsed values, or error:
-// notEnoughArgsErr when there are less than two arguments
-// parseFailedErr when one of the two
-func parseArguments(args []string) (float64, float64, error) {
-	var a, b float64
-	if len(args) < 2 {
-		return a, b, notEnoughArgsErr
-	}
-	// use loop and return a slice if the number of arguments to sum becomes higher
-	a, err := parseValue(args[0])
-	if err != nil {
-		return a, b, err
-	}
-	b, err = parseValue(args[1])
-	if err != nil {
-		return a, b, err
-	}
-	return a, b, nil
-}
-
-func parseValue(val string) (float64, error) {
-	panic("Not implemented")
 }
--- pkg/adder/main.go
+++ pkg/adder/main.go
@@ -1,5 +1,50 @@
 package adder
 
-func AddNumbers(a, b float64) float64 {
+import (
+	"errors"
+	"fmt"
+	"math/big"
+)
+
+const precision = 100
+
+var NotEnoughArgsErr = errors.New("Not enough arguments")
+
+type ParseError struct {
+	value string
+}
+
+func (pe ParseError) Error() string {
+	return fmt.Sprintf("Invalid float value: %s", pe.value)
+}
+
+// ParseArguments takes first two arguments and tries to parse them
+// into floats
+// return parsed values, or error:
+// notEnoughArgsErr when there are less than two arguments
+// parseFailedErr when one of the two
+func ParseArguments(args []string) (*big.Float, *big.Float, error) {
+	var a, b *big.Float
+	if len(args) < 2 {
+		return a, b, NotEnoughArgsErr
+	}
+	// use loop and return a slice if the number of arguments to sum becomes higher
+	a, err := parseValue(args[0])
+	if err != nil {
+		return a, b, err
+	}
+	b, err = parseValue(args[1])
+	if err != nil {
+		return a, b, err
+	}
+	return a, b, nil
+}
+
+func parseValue(s string) (*big.Float, error) {
+	res, _, err := big.ParseFloat(s, 0, precision, big.ToNearestEven)
+	return res, err
+}
+
+func AddNumbers(a, b *big.Float) *big.Float {
 	panic("Not implemented")
 }
