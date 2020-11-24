8c5516bfaf67cb5b7fcbac6b9d4e823775171a13
Odohi David <odohi.david@gmail.com>
2020-11-24 02:34:06 +0100

    implement the sum method to sum numbers passed to it from the command line and use only the first two numbers from the command line.

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Sum recieves a range of values and returns their sum
+// this method can accept variadic number of arguments if we eventually want to do so
+func Sum(values ...string) (float64, error) {
+	sum := 0.0
+	for _, arg := range values {
+		argFloat64, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			return 0.0, fmt.Errorf("cannot convert %s to number", arg)
+		}
+		sum += argFloat64
+	}
+	return sum, nil
+}
+
+func main() {
+	argsFromCommand := os.Args[1:]
+	if len(argsFromCommand) == 0 {
+		log.Println("To use this command, you must pass at least one number as argument.")
+		return
+	}
+	// "Take two numbers from the command line"
+	twoNumbers := argsFromCommand[:2]
+	sum, err := Sum(twoNumbers...)
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+	fmt.Printf("sum of %v: %f\n", strings.Join(twoNumbers, " and "), sum)
+}
