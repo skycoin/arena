fdbe3642aafc644c80401f7d24b85535de31c258
Udaya N <udayaprakash2899@gmail.com>
2020-11-27 03:43:49 +0530

    Refactored AddCommand() to allow positional arguments instead of named

--- cmd/add.go
+++ cmd/add.go
@@ -3,29 +3,52 @@ package cmd
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 )
+
+var out io.Writer = os.Stdout
+
+var errCannotConvert = fmt.Errorf("Cannot convert first argument to number")
+var errTwoArguments = fmt.Errorf("Exactly two arguments required to add")
 
 // AddCommand adds two numbers given as arguments
 func AddCommand() {
 	addCommand := flag.NewFlagSet("add", flag.ExitOnError)
 
-	var firstNum, secondNum int
-
-	// Inbuilt flag package does not support positional arguments, so arguments are named
-	addCommand.IntVar(&firstNum, "first", 0, "First Number to add. (Required)")
-	addCommand.IntVar(&secondNum, "second", 0, "Second Number to add. (Required)")
-
 	// Two arguments are required, exit if one of them is not provided
 	if len(os.Args) < 2 {
-		fmt.Printf("Enter required arguments\n")
+		fmt.Fprintf(out, "Enter a valid command\n")
 		addCommand.Usage()
 		os.Exit(1)
 	}
 
 	if err := addCommand.Parse(os.Args[2:]); err != nil {
-		fmt.Printf("Cannot parse, %v", err)
+		fmt.Fprintf(out, "Cannot parse, Err: %v", err)
 	}
 
-	fmt.Printf("%v + %v = %v\n", firstNum, secondNum, firstNum+secondNum)
+	sum, err := add(addCommand.Args())
+	if err != nil {
+		fmt.Fprintf(out, "Cannot add, Err: %v", err)
+	} else {
+		fmt.Fprintln(out, sum)
+	}
+}
+
+func add(args []string) (int, error) {
+	if len(args) != 2 {
+		return 0, errTwoArguments
+	}
+
+	firstNum, err := strconv.Atoi(args[0])
+	if err != nil {
+		return 0, errCannotConvert
+	}
+	secondNum, err := strconv.Atoi(args[1])
+	if err != nil {
+		return 0, errCannotConvert
+	}
+
+	return firstNum + secondNum, nil
 }
